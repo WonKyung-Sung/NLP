5e9aadd59f8c9e5e0b0d95807925c41bbecadf60
WonKyung-Sung <75463723+WonKyung-Sung@users.noreply.github.com>
2020-12-04 14:28:57 +0900

    Update Main.py

--- utils/Main.py
+++ utils/Main.py
@@ -1,197 +1,125 @@
-#-*- coding: utf-8 -*-
-
-import re
-import os
-import sys
-import pickle
-import numpy as np
+# -*- coding: utf-8 -*-
+#import tensorflow as tf
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-sys.path.append("../")
-parent_path = os.path.abspath('.')
-import text_preprocess as wk
-# import Model as WKM
+import time
+import numpy as np
+import time
+'''
+part1
+'''
 
-class learning:
-    def __init__(self):
-        self.data = False
-        self.model = False
-        self.voca_dic = False
-        self.max_len_voca = False
-        self.max_len_char = False
-        self.tokenizer = False
-        
-    def preprocess(self, data, input_col="input", label_col="label", version="char3"):
-        if label_col not in data.keys():
-            raise Exception('label 컬럼이 존재하지 않습니다.') 
-        if input_col not in data.keys():
-            raise Exception('input 컬럼이 존재하지 않습니다.')
-            
-        self.tokenizer = eval("wk.sent2" + version)
-        try: 
-            with open(parent_path + "/data/voca_dic_" + version + ".pickle", 'rb') as f:
-                self.voca_dic = pickle.load(f)
+# search = open("C:/Users/W.K.SUNG/Desktop/Bin/naver2014_05_12.txt",'w')
+chrome_path = r"/data3/home/wonkyung.sung/Crome_driver/chromedriver"
+driver = webdriver.Chrome(chrome_path)
+df = pd.DataFrame()
+import urllib
+query = ["세탁기", "노트북", "건조기", "티비", "스마트폰", "모니터"]
+
+# query = ["삼성"]
+query_ = [urllib.parse.quote(i) for i in query]
+
+
+for query_first in query:
+    print(query_first, " 시작")
+
+    driver.get("https://shopping.naver.com/")
+    driver.find_element_by_xpath("""//*[@id="autocompleteWrapper"]/input[1]""").send_keys(query_first)
+    driver.find_element_by_xpath("""//*[@id="autocompleteWrapper"]/a[2]""").click()
+
+    tmp_df = pd.DataFrame()
+
+    # 크롤링
+    time.sleep(5)
+    driver.find_element_by_xpath("""//*[@id="__next"]/div/div[2]/div/div/div[1]/div[1]/ul/li[2]""").click()
+
+
+    next_page_ =True 
+    count = 0 
+         
+    while next_page_:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+        # e = driver.find_elements_by_xpath("""//*[@id="__next"]/div/div[2]/div/div[4]/div[1]/ul/div/div""")
+        try:
+            e = driver.find_element_by_class_name("list_basis").find_elements_by_tag_name("li")
         except:
-            print("""기존 사전이 존재하지 않습니다. \n현 데이터 기반으로 새로 생성합니다.""")
+            continue
+        #print(str(len(e)), "개를 긁었습니다.")
+        # count = 0 
+        count_o = 0 
+        for th in e:
+            try:
+                th.find_element_by_class_name("basicList_etc_box__1Jzg6")
+
+                raw = th.text
+
+                # 제품 카테고리
+                product_lines = th.find_element_by_class_name("basicList_depth__2QIie").find_elements_by_tag_name("a")
+                product_line = ";;".join([product_line.text for product_line in product_lines])
+
+                # 제품명
+                product_name = th.find_element_by_class_name("basicList_title__3P9Q7").find_element_by_tag_name("a").text
+
+                # 가격
+                # product_name = th.find_element_by_class_name("basicList_price_2r23")
+
+                price_infos = th.find_element_by_class_name("basicList_price__2r23_")
+                price = price_infos.find_element_by_class_name("price_num__2WUXn").text
+                number_of_store = price_infos.find_element_by_tag_name("a").text.replace("판매처 ", "")
+                detailes = th.find_elements_by_class_name("basicList_detail__27Krk")
+                detailes = ";;".join([tmp.text for tmp in detailes])
+                review_info = th.find_element_by_class_name("basicList_etc_box__1Jzg6")
+                review_url = review_info.find_element_by_tag_name("a").get_attribute('href')
+                review_star = review_info.find_element_by_class_name("basicList_star__3NkBn").text.replace("별점 ", "")
+                review_number_of = review_info.find_element_by_class_name("basicList_num__1yXM9").text
+
+                tmp_df = tmp_df.append(pd.DataFrame({"product_name":[product_name],
+                                                     "검색명" : [query_first],
+                                                     "제품카테고리":[product_line],
+                                                     "price": [price],
+                                                     "판매처": [number_of_store],
+                                                     "detailes": [detailes],
+                                                     "별점":[review_star],
+                                                     "review_url":[review_url],
+                                                     "리뷰수":[review_number_of],
+                                                     "raw":[raw]}), ignore_index=True)
+
+                # count_o += 1 
+                # print("   ", str(count_o))
+
+            except:
+                # count+= 1
+                # print(count)
+                pass
+        df = df.append(tmp_df, ignore_index=True)
         
-        data["preprocess"] = self.tokenizer(data[input_col])
-        if self.voca_dic == False:
-            self.voca_dic = wk.vocabulary_maker(data['preprocess'])
-        
-        self.max_len_voca = len(self.voca_dic)
-        print(len(self.voca_dic))
-        self.max_len_char = data['preprocess'].map(len).max()
-        
-#         data['input_'] = data.preprocess.map(lambda x:
-#                                              np.array(
-#                                                  [self.voca_dic.get(word, 0) for word in x])
-#                                             )
-        data['input_'] = data.preprocess.map(lambda x: np.pad( 
-            np.array([self.voca_dic.get(word, 0) for word in x]) ,(0,self.max_len_char-len(x)),
-            'constant', constant_values=(0,0)))
-        if label_col != "label":
-            data["label"] = data[input_col]
-        self.data =data
+        df = df.drop_duplicates()
 
-    def predict_preprocess(self, data):
-        if type(data) == str:
-            data = pd.DataFrame({"input":[data]})
-        else:
-            data =pd.DataFrame({"input":data})
-            
-        data["preprocess"] = self.tokenizer(data["input"])    
-#         data['input_'] = data.preprocess.map(
-#             lambda x: np.array(
-#                 [self.voca_dic.get(word, 0) for word in x]
-#             )
-#         )
-        self.max_len_char = data['preprocess'].map(len).max()
-        data['input_'] = data.preprocess.map(lambda x: np.pad( 
-           np.array([self.voca_dic.get(word, 0) for word in x]) ,(0,self.max_len_char-len(x)),
-           'constant', constant_values=(0,0)))
-        return data
-        
-    def predict(self, data):
-        data = self.predict_preprocess(data)
-        data = np.stack(data.input_, axis=0)
-        data = tf.data.Dataset.from_tensor_slices(data).batch(512)
-        return self.model.predict(data)
-
-    def select_model(self, selected_model="TextCnn_char", scrach=True):
-        if selected_model == "TextCnn_char":
-            print(1)
-            if scrach:
-                import Model as wkm
-                self.model = wkm.TextCnn_char(max_len_voca = self.max_len_voca)
-            else:
-                self.model = tf.keras.models.load_model('./path/to/location')
-        if selected_model == "TextCnn_syllable":
-            print(2)
-            if scrach:
-                import Model as wkm
-                self.model = wkm.TextCnn_syllable(max_len_voca = self.max_len_voca)
-            else:
-                self.model = tf.keras.models.load_model('./path/to/location')
-
-    def fit(self, test_ratio = 0.1, batch_size = 512, EPOCHS = 1):
-        # 학습 나누기 
-        raw_y = self.data[["label"]].to_numpy()
-        raw_x = np.stack(self.data.input_, axis=0)
-        x_train, x_test, y_train, y_test = train_test_split(raw_x, raw_y,  test_size = test_ratio,random_state=43)
-        train_ds = tf.data.Dataset.from_tensor_slices(
-            (x_train, y_train)).shuffle(x_train.shape[0] +1).batch(batch_size)
-        test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
-
-        loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-        optimizer = tf.keras.optimizers.Nadam()
-        train_loss = tf.keras.metrics.Mean(name='train_loss')
-        train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
-
-        test_loss = tf.keras.metrics.Mean(name='test_loss')
-        test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
-
-        @tf.function
-        def train_step(batch_input, labels):
-            with tf.GradientTape() as tape:
-                predictions = self.model(batch_input)
-                loss = loss_object(labels, predictions)
-                gradients = tape.gradient(loss, self.model.trainable_variables)
-                optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-
-                train_loss(loss)
-                train_accuracy(labels, predictions)
-
-        @tf.function
-        def test_step(batch_input, labels):
-            predictions = self.model(batch_input)
-            t_loss = loss_object(labels, predictions)
-
-            test_loss(t_loss)
-            test_accuracy(labels, predictions)
-
-        for epoch in range(EPOCHS):
-            for batch_input, labels in train_ds:
-                train_step(batch_input, labels)
-
-
-            for test_batch_input, test_labels in test_ds:
-                test_step(test_batch_input, test_labels)
-
-            template = '에포크: {}, 손실: {}, 정확도: {}, 테스트 손실: {}, 테스트 정확도: {}'
-            print (template.format(epoch+1,
-                             train_loss.result(),
-                             train_accuracy.result()*100,
-                             test_loss.result(),
-                             test_accuracy.result()*100))
-            # model.save('path/to/location')
-
-    def data(self):
-        return self.data
-    def model(self):
-        return self.model
-    def data(self):
-        return self.voca_dic
-    
-    def save(self, output_path):
-        if not os.path.isdir(output_path +"model/"): 
-            os.makedirs(output_path +"model/")
-        self.model.save(output_path + "model")
-        if not os.path.isdir(output_path +"class_info/"): 
-            os.makedirs(output_path +"class_info/")
-            
-        pickle.dump(self.voca_dic, open(output_path +"class_info/"+"voca_dic.pickle", "wb"))
-        pickle.dump(self.max_len_voca, open(output_path +"class_info/"+"max_len_voca.pickle", "wb"))
-        pickle.dump(self.max_len_char, open(output_path +"class_info/"+"max_len_char.pickle", "wb"))
-        pickle.dump(self.tokenizer, open(output_path +"class_info/"+"tokenizer.pickle", "wb"))
-        
-    def load(self, output_path):
-        self.model = tf.keras.models.load_model(output_path + "model")
-        self.voca_dic = pickle.load(open(output_path +"class_info/"+"voca_dic.pickle", "rb"))
-        self.max_len_voca = pickle.load(open(output_path +"class_info/"+"max_len_voca.pickle", "rb"))
-        self.max_len_char = pickle.load(open(output_path +"class_info/"+"max_len_char.pickle", "rb"))
-        self.tokenizer = pickle.load(open(output_path +"class_info/"+"tokenizer.pickle", "rb"))
-        
-    def lime(self, text):
-        from lime.lime_text import LimeTextExplainer
-        explainer = LimeTextExplainer(class_names=["부정","긍정"])
-        max_len = len(text.split(" "))
-        explanation = explainer.explain_instance(text, classifier_fn = self.predict, labels=(1,), num_features=max_len)
-        explanation.show_in_notebook(text=True)
-        return explanation.as_list()
-    
-    def report(self, pred, labels, target_names=False):
-        '''
-        pred = [0, 0, 2, 2, 1]
-        labels = [0, 1, 2, 2, 2]
-        target_names = ['class 0', 'class 1', 'class 2']       
-        '''
-        if np.array(pred).shape[-1] == 2:
-            pred = np.argmax(pred, 1)
-        
-        from sklearn.metrics import classification_report
-        if target_names:
-            print(classification_report(labels, pred, target_names=target_names))
-        else:
-            print(classification_report(labels, pred))
-        
+        next_page_sum = ""
+        for next_page_tmp in driver.find_element_by_xpath("""//*[@id="__next"]/div/div[2]/div[2]/div[4]/div[1]/div[3]""").find_elements_by_tag_name("a"):
+            # //*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[3] # 이거 
+            # //*[@id="__next"]/div/div[2]/div/div[3]/div[1]/div[3]
+            if next_page_tmp.text == "다음":
+                next_page_sum = next_page_sum + "다음"
+                next_page_tmp.send_keys(Keys.ENTER)
+                count+= 1
+                print(count)
+                if (query_first is not ["삼성", "엘지"])& (count > 300):
+                    print(1)
+                    next_page_ = False
+        if "다음" not in next_page_sum:
+            next_page_ = False
+    df = df.drop_duplicates()
+    df.drop_duplicates().to_csv("제품명_1101_" + query_first +".csv", index=False)
